b0a799251e8cbd13dbc9f5a5c2cd5dd0b83e2873
Danil Usoltsev <danilusoltsev05@gmail.com>
2025-09-21 23:26:38 +0300

    ruff fix

--- tests/autotests/test_task03.py
+++ tests/autotests/test_task03.py
@@ -13,7 +13,11 @@ from rpq_concrete_cases import CASES_RPQ, CaseRPQ
 # Fix import statements in try block to run tests
 try:
     from project.build_graph import regex_to_dfa
-    from project.tensor_automata import intersect_automata, AdjacencyMatrixFA, tensor_based_rpq
+    from project.tensor_automata import (
+        intersect_automata,
+        AdjacencyMatrixFA,
+        tensor_based_rpq,
+    )
 except ImportError:
     pytestmark = pytest.mark.skip("Task 3 is not ready to test!")
 
